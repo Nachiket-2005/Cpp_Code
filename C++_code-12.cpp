b3212dccf7887df437b00a43e9959efd34f1bd3e
NACHIKET PRAJAPATI <119723524+Nachiket-2005@users.noreply.github.com>
2023-04-09 18:35:58 +0530

    Square of Num

---
+++ C++_code-12.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<math.h>
+
+int main(){
+    int a,c;
+    std :: cout<<" Enter The Num\n ";
+    std :: cin>>a;
+    c = pow(a,2);
+    std :: cout<<" Square of "<<a<<" is "<<c;
+}
